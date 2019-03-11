8df2b7f00ecaccf83b27ca8b3917531c4345753c
Drew Stone <drewstone329@gmail.com>
2019-03-12 00:02:18 +0200

    Adds encrypt functionality for testing and example usage

--- src/crypto.ts
+++ src/crypto.ts
@@ -2,8 +2,24 @@ import CryptoJS from 'crypto-js';
 import AES from 'crypto-js/aes';
 import SHA256 from 'crypto-js/sha256'
 
+const KEY = 'commonwealth-identity-service';
+
+export const encrypt = (data) => {
+    if (!data.hasOwnProperty('idHash') ||
+      !data.hasOwnProperty('githubAccount') ||
+      !data.hasOwnProperty('identityHash') ||
+      !data.hasOwnProperty('sender')
+    ) {
+      console.log(`Data is inproperly formatted: ${data}`);
+      process.exit(-1);
+    } else {
+      const cipherText = AES.encrypt(data, KEY).toString();
+      return cipherText;
+    }
+}
+
 export const decrypt = (data) => {
-    const bytes  = AES.decrypt(data, 'commonwealth-identity-service');
+    const bytes = AES.decrypt(data, KEY);
     return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 }
 
@@ -12,17 +28,17 @@ export const verifyAttestationWithGist = (gistId, txSender, response) => {
     let decryptedData = decrypt(content);
     // Check sufficient conditions for verifying attestations
     if (decryptedData.idHash != SHA256(gistId) ||
-        decryptedData.author != response.owner.login ||
-        decryptedData.sender != txSender)
-    {
-        return {
-            identityHash: decryptedData.identityHash,
-            success: true,
-        };
+      decryptedData.githubAccount != response.owner.login ||
+      decryptedData.sender != txSender
+    ) {
+      return {
+        identityHash: decryptedData.identityHash,
+        success: true,
+      };
     } else {
-        return {
-            identityHash: decryptedData.identityHash,
-            success: false,
-        };
+      return {
+        identityHash: decryptedData.identityHash,
+        success: false,
+      };
     }
 }
--- src/process.ts
+++ src/process.ts
@@ -23,3 +23,5 @@ const start = async () => {
     }, INTERVAL_LENGTH)
   });
 }
+
+start();
