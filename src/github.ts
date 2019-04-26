6efebcb9503ca5643390e4f02b7efc2ddfa65f20
Drew Stone <drewstone329@gmail.com>
2019-04-26 19:26:17 +0200

    Fail verification on incorrect attestation link

--- src/github.ts
+++ src/github.ts
@@ -18,7 +18,32 @@ export const processAttestEvent = async (remoteUrlString: string, event) => {
   let gistId = event.attestation;
   let txSender = event.sender;
 
-  const response = await axios(getRequestOptions(gistId));
+  let response;
+  try {
+    response = await axios(getRequestOptions(gistId));  
+  } catch (error) {
+    // Error
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("Request and response sent")
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    } else if (error.request) {
+        console.log("No response sent")
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+    }
+    console.log(error.config);
+    return false;
+  }
+  
   return (response.status !== 200)
     ? false
     : verifyAttestationWithGist(gistId, txSender, response.data);
